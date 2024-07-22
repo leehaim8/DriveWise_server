3821c12415e7d6e7cba7946a7254a0023ac3cbb1
leehaim8 <leehaim811@gmail.com>
2024-07-22 16:33:43 +0300

    fixing the menu that every user will have the menu that belongs to him

--- client/js/script.js
+++ client/js/script.js
@@ -7,3 +7,31 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", async function() {
+    const userId = sessionStorage.getItem('user_id');
+    const userType = sessionStorage.getItem('user_type');
+
+    const response = await fetch(`http://127.0.0.1:8080/api/users/${userId}`
+    );
+    const userData = await response.json();
+    displayUserSpecificData(userData, userType);
+});
+
+function displayUserSpecificData(userData, userType) {
+    const feedbackElements = document.querySelectorAll(".feedback");
+    const addSimulationElements = document.querySelectorAll(".addSimulation");
+    const studentsElements = document.querySelectorAll(".Students");
+
+    if (userType === 'Teacher') {
+        feedbackElements.forEach(el => el.hidden = true);
+        addSimulationElements.forEach(el => el.hidden = false);
+        studentsElements.forEach(el => el.hidden = false);
+    } else {
+        feedbackElements.forEach(el => el.hidden = false);
+        addSimulationElements.forEach(el => el.hidden = true);
+        studentsElements.forEach(el => el.hidden = true);
+    }
+}
+
