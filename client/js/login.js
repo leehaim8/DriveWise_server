7d02792ea0e931bd049e1ed6db246cce1b5e2c3f
leehaim8 <leehaim811@gmail.com>
2024-07-22 16:30:42 +0300

    create login page

---
+++ client/js/login.js
@@ -0,0 +1,27 @@
+document.getElementById("login-form").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    console.log(username);
+    const password = document.getElementById("password").value;
+    console.log(password);
+
+    const response = await fetch("http://127.0.0.1:8080/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+        sessionStorage.setItem('user_id', result.userId);
+        sessionStorage.setItem('user_type', result.userType);
+        window.location.href = 'index.html';
+
+    } else {
+        document.getElementById("error-message").textContent = result.error;
+    }
+});
